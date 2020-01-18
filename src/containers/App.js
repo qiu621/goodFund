8de544816a4ae22c8c0134f68f6b24a935f302d4
Andy Zou <andyzou_jiaming@berkeley.edu>
2020-01-18 14:07:35 -0800

    web3

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,63 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import web3 from '../blockchain/web3';
+import Header from '../components/Header/Header';
+import ProjectList from '../components/ProjectList/ProjectList';
 import NavBar from './Nav/Nav.js'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useParams,
+  Link
+} from "react-router-dom";
+
 
 class App extends Component {
+
+  constructor(props) {
+
+    super(props);
+
+    this.state = {
+      account: null,
+      projectData: [],
+      newProject: { isLoading: false },
+    };
+
+  }
+
+  componentWillMount() {
+    web3.eth.getAccounts().then((accounts) => {
+      [this.state.account] = accounts;
+      this.getProjects();
+    });
+  }
+
+  getProjects() {
+    
+  }
+
+
   render () {
     return (
-      <React.Fragment>
-      <Router>
-
-      <NavBar />
-      <div class="jumbotron">
-  <h1 class="display-4">Hello, world!</h1>
-  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-  <hr class="my-4"/>
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <p class="lead">
-    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-  </p>
-</div>
-      <div class="card-group">
-  <div class="card">
-    <img class="card-img-top" src="..." alt="Card image cap"/>
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="..." alt="Card image cap"/>
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-  <div class="card">
-    <img class="card-img-top" src="..." alt="Card image cap"/>
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-muted">Last updated 3 mins ago</small>
-    </div>
-  </div>
-</div>
-      </Router>
-
-      </React.Fragment>
+      <div className="App">
+        <header className="App-header">
+          <Header />
+          <Link to="/new"><div>NEW PROJECT</div></Link>
+          <div>...</div>
+          <ProjectList projects={this.state.projectData} />
+        </header>
+      </div>
     );
-
   }
 }
 
