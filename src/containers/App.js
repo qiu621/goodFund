d7243c00859faa19493377b7506c790f938a9ec9
Andy Zou <andyzou_jiaming@berkeley.edu>
2020-01-18 14:19:26 -0800

    restructuring

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import web3 from '../blockchain/web3';
 import Header from '../components/Header/Header';
 import ProjectList from '../components/ProjectList/ProjectList';
+import NewProject from './NewProject';
+import ProjectPage from './ProjectPage';
 import NavBar from './Nav/Nav.js'
 import {
   BrowserRouter as Router,
@@ -41,16 +43,38 @@ class App extends Component {
 
   render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <Header />
-          <Link to="/new"><div>NEW PROJECT</div></Link>
-          <div>...</div>
-          <ProjectList projects={this.state.projectData} />
-        </header>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <App />
+          </Route>
+          <Route path="/new">
+            <NewProject />
+          </Route>
+          <Route path="/:title">
+            <ProjectPost />
+          </Route>
+        </Switch>
+      </Router>
+
+      // <div className="App">
+      //   <header className="App-header">
+      //     <Header />
+      //     <Link to="/new"><div>NEW PROJECT</div></Link>
+      //     <div>...</div>
+      //     <ProjectList projects={this.state.projectData} />
+      //   </header>
+      // </div>
     );
   }
 }
+
+function ProjectPost() {
+  let { title } = useParams();
+  return (
+    <ProjectPage />
+  );
+}
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,43 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
-import NewProject from './components/NewProject/NewProject';
-import ProjectPage from './components/Project/ProjectPage';
 import * as serviceWorker from './serviceWorker';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    useParams,
-    Link
-  } from "react-router-dom";
+
+
 
 ReactDOM.render(
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <App />
-        </Route>
-        <Route path="/new">
-          <NewProject />
-        </Route>
-        <Route path="/:title">
-          <ProjectPost />
-        </Route>
-      </Switch>
-    </Router>,
-    document.getElementById('root')
+    <App />, document.getElementById('root')
   );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-
-function ProjectPost() {
-  let { title } = useParams();
-  return (
-    <ProjectPage />
-  );
-}
