33dc60d4ec2d3775a2c76109743a346d228ee219
qiu621 <jeffreyqiu123@gmail.com>
2020-01-19 02:40:45 -0800

    refactoring

--- src/blockchain/crowdfundInstance.js
+++ src/blockchain/crowdfundInstance.js
@@ -1,100 +1,101 @@
 /* eslint-disable */
 import web3 from './web3';
 
-const address = '0x0E88962aB7D5FC6E3c6FF479C9791Ccf5c15F7A1'; // Your deployed contract's address goes here
+const address = '0x4151b88feee5110006C39F4bc92c47481cF9F80D'; // Your deployed contract's address goes here
 // Example:
 // const address = '0x09r80cnasjfaks93m9v2';
 
-const abi = [
-	{
-		"constant": false,
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "title",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "description",
-				"type": "string"
-			},
-			{
-				"internalType": "uint256",
-				"name": "numberOfDays",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "goal",
-				"type": "uint256"
-			}
-		],
-		"name": "startProject",
-		"outputs": [],
-		"payable": false,
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": false,
-				"internalType": "address",
-				"name": "contractAddress",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"internalType": "address payable",
-				"name": "projectStarter",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"internalType": "string",
-				"name": "projectTitle",
-				"type": "string"
-			},
-			{
-				"indexed": false,
-				"internalType": "string",
-				"name": "projectDesc",
-				"type": "string"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "deadline",
-				"type": "uint256"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "goalAmount",
-				"type": "uint256"
-			}
-		],
-		"name": "ProjectStarted",
-		"type": "event"
-	},
-	{
-		"constant": true,
-		"inputs": [],
-		"name": "returnAllProjects",
-		"outputs": [
-			{
-				"internalType": "contract Project[]",
-				"name": "",
-				"type": "address[]"
-			}
-		],
-		"payable": false,
-		"stateMutability": "view",
-		"type": "function"
-	}
-]; // Your ABI goes here (Crowdfunding contract)
+const abi = 
+	[
+		{
+			"constant": false,
+			"inputs": [
+				{
+					"internalType": "string",
+					"name": "title",
+					"type": "string"
+				},
+				{
+					"internalType": "string",
+					"name": "description",
+					"type": "string"
+				},
+				{
+					"internalType": "uint256",
+					"name": "numberOfDays",
+					"type": "uint256"
+				},
+				{
+					"internalType": "uint256",
+					"name": "project_goal",
+					"type": "uint256"
+				}
+			],
+			"name": "startProject",
+			"outputs": [],
+			"payable": false,
+			"stateMutability": "nonpayable",
+			"type": "function"
+		},
+		{
+			"anonymous": false,
+			"inputs": [
+				{
+					"indexed": false,
+					"internalType": "address",
+					"name": "contractAddress",
+					"type": "address"
+				},
+				{
+					"indexed": false,
+					"internalType": "address payable",
+					"name": "projectStarter",
+					"type": "address"
+				},
+				{
+					"indexed": false,
+					"internalType": "string",
+					"name": "projectTitle",
+					"type": "string"
+				},
+				{
+					"indexed": false,
+					"internalType": "string",
+					"name": "projectDesc",
+					"type": "string"
+				},
+				{
+					"indexed": false,
+					"internalType": "uint256",
+					"name": "project_deadline",
+					"type": "uint256"
+				},
+				{
+					"indexed": false,
+					"internalType": "uint256",
+					"name": "goalAmount",
+					"type": "uint256"
+				}
+			],
+			"name": "ProjectStarted",
+			"type": "event"
+		},
+		{
+			"constant": true,
+			"inputs": [],
+			"name": "returnAllProjects",
+			"outputs": [
+				{
+					"internalType": "contract Project[]",
+					"name": "",
+					"type": "address[]"
+				}
+			],
+			"payable": false,
+			"stateMutability": "view",
+			"type": "function"
+		}
+	]; // Your ABI goes here (Crowdfunding contract)
 // Example:
 // const abi = [
 //     {
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -20,7 +20,7 @@ function Header() {
       		<NavBar />
       		<div class="jumbotron" style={divStyle}>
           <div class="cover-container" style={coverContainer}>
-        			<h2 class="display-4">Crowdfuding for Good</h2>
+        			<h2 class="display-4">Crowdfunding for Good</h2>
         			<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
             </div>
       		</div>
