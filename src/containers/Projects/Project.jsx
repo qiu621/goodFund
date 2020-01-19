ded820895ebf3aee9a7d6ccf0e1ccf73126529ae
kevin-thankyou-lin <kevin.lin20000@gmail.com>
2020-01-18 19:13:55 -0800

    collect information from new project creation

---
+++ src/containers/Projects/Project.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Button from 'react-bootstrap/Button'
+
+
+
+
+const ProjectHeader = () => (
+  <Jumbotron>
+    <h1>Hello, world!</h1>
+    <p>
+      This is a simple hero unit, a simple jumbotron-style component for calling
+      extra attention to featured content or information.
+    </p>
+    <p>
+      <Button variant="primary">Learn more</Button>
+    </p>
+  </Jumbotron>
+
+
+
+);  
+
+export default ProjectHeader;
+
+// export default ProjectHeader
